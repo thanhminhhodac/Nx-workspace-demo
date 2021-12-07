44bc26ef9a1989600832320947eaae46fa6c8327
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 15:22:39 +0700

    create header for admin application

--- apps/admin-app/src/app/app.jsx
+++ apps/admin-app/src/app/app.jsx
@@ -2,9 +2,12 @@
 import React from 'react';
 // shared libs
 import { Footer } from '@thanhminh-org/shared/feature/footer';
+// self feature libraries
+import { Header } from '@thanhminh-org/admin/feature/header';
 
 const App = () => (
   <div className="app">
+    <Header />
     <Footer />
   </div>
 );
