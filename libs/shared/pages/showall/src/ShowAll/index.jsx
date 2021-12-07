021185c49dc03d93a3b024b036c48063e1ebea6a
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 23:15:29 +0700

    create shared pages

---
+++ libs/shared/pages/showall/src/ShowAll/index.jsx
@@ -0,0 +1,14 @@
+// libs
+import React from 'react';
+// style
+import './style.scss';
+
+export const ShowAll = () => {
+  return (
+    <div className="show-all-wrapper">
+      <div className="show-all-wrapper-inner">
+        <h1 className="show-all-title"> All Todos</h1>
+      </div>
+    </div>
+  );
+};
