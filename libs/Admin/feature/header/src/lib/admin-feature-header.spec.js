c9cb5219964e534ff9d1145b0179964867b533ab
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 20:50:16 +0700

    delete admin lib

--- libs/Admin/feature/header/src/lib/admin-feature-header.spec.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Header from './admin-feature-header';
-describe('AdminFeatureHeader', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Header />);
-    expect(baseElement).toBeTruthy();
-  });
-});
--- libs/Admin/pages/dashboard/src/lib/admin-pages-dashboard.spec.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Dashboard from './admin-pages-dashboard';
-describe('AdminPagesDashboard', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Dashboard />);
-    expect(baseElement).toBeTruthy();
-  });
-});
