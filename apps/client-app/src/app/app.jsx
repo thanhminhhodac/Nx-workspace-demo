8aa8f18fb7908d45f282a606a2c171dbd7aa7086
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 17:08:11 +0700

    add routes to client app

--- apps/client-app/src/app/app.jsx
+++ apps/client-app/src/app/app.jsx
@@ -1,13 +1,23 @@
 // libs
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // share libs
 import { Footer } from '@thanhminh-org/shared/feature/footer';
 // self libs
 import { Header } from '@thanhminh-org/client/feature/header';
+import { routes } from '../config/routes.jsx';
+
 const App = () => (
   <div className="app">
-    <Footer />
-    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Footer />
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+        <Header />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 
