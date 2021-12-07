bc0c74011e4c361235f701f9f3e80a1312465096
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 21:26:48 +0700

    create shared ui HeaderNavLink

---
+++ libs/shared/ui/header-navlink/src/HeaderNavLink/index.jsx
@@ -0,0 +1,18 @@
+// libs
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+/**
+ * @param {Object} props
+ * @param {string} props.path
+ * @param {string} props.label
+ * @returns {React.ReactElement}
+ */
+
+export const HeaderNavLink = ({ path, label }) => (
+  <div className="navbar-link-wrapper">
+    <Link to={path}> {label} </Link>;
+  </div>
+);
+
+export default HeaderNavLink;
