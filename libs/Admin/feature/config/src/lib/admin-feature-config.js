7203efc9cd7f6f3d18e33234ee0ba0d8493114cd
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 16:00:55 +0700

    add dashboard to routes config in admin application

--- libs/Admin/feature/config/src/lib/admin-feature-config.js
+++ libs/Admin/feature/config/src/lib/admin-feature-config.js
@@ -1,4 +1,12 @@
 // share routes
 import { shareRoutes } from '@thanhminh-org/shared/feature/config';
+// self routes
+import { Dashboard } from '@thanhminh-org/admin/pages/dashboard';
 
-export const routes = [...shareRoutes, {}];
+export const routes = [
+  ...shareRoutes,
+  {
+    path: '/dashboard',
+    component: Dashboard,
+  },
+];
