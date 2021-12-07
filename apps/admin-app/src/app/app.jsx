16b9cafb739ddef1fc2fd1de25baa482c73cac94
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-07 15:13:44 +0700

    using shared feature Footer in admin-app

---
+++ apps/admin-app/src/app/app.jsx
@@ -0,0 +1,12 @@
+// libs
+import React from 'react';
+// shared libs
+import { Footer } from '@thanhminh-org/shared/feature/footer';
+
+const App = () => (
+  <div className="app">
+    <Footer />
+  </div>
+);
+
+export default App;
