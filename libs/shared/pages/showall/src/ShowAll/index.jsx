10d2893c4a8a46aee30df2afc89d86abfdbd7e3d
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-12-08 00:02:50 +0700

    create dataSource util libs

--- libs/shared/pages/showall/src/ShowAll/index.jsx
+++ libs/shared/pages/showall/src/ShowAll/index.jsx
@@ -1,13 +1,31 @@
 // libs
 import React from 'react';
+import { List, Tag } from 'antd';
 // style
 import './style.scss';
+// other
+import { todos } from '@thanhminh-org/shared/util/data-sources';
 
 export const ShowAll = () => {
   return (
     <div className="show-all-wrapper">
       <div className="show-all-wrapper-inner">
         <h1 className="show-all-title"> All Todos</h1>
+        <div className="show-all-body">
+          <List
+            className="todo-list"
+            itemLayout="horizontal"
+            dataSource={todos}
+            renderItem={(item) => (
+              <List.Item key={item.id} className="todo-item">
+                <Tag className="todo-item-status" color={item.status.value}>
+                  {item.status.label}
+                </Tag>
+                <h2 className="todo-item-title">{item.title} </h2>
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
     </div>
   );
